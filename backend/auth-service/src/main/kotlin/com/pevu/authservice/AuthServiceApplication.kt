0823153ff17532b68a1400c029b0bbe3f2eee5e2
Eugene <eugenemoyale451@gmail.com>
2025-07-16 21:39:29 +0300

    Debug: print DATABASE_URL and update config

--- backend/auth-service/src/main/kotlin/com/pevu/authservice/AuthServiceApplication.kt
+++ backend/auth-service/src/main/kotlin/com/pevu/authservice/AuthServiceApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class AuthServiceApplication
 
 fun main(args: Array<String>) {
+    println("DATABASE_URL env: " + System.getenv("DATABASE_URL"))
     runApplication<AuthServiceApplication>(*args)
 } 
